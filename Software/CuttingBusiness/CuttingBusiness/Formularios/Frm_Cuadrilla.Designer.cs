600f564ebba1b28e2b18f294b6b5e4ea0971bc16
Juan Alvaro Esquivel Aguilar <iscjaea@gmail.com>
2019-11-27 17:56:21 -0600

    Cambios

--- Software/CuttingBusiness/CuttingBusiness/Formularios/Frm_Cuadrilla.Designer.cs
+++ Software/CuttingBusiness/CuttingBusiness/Formularios/Frm_Cuadrilla.Designer.cs
@@ -76,7 +76,7 @@
             this.panelControl1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelControl1.Location = new System.Drawing.Point(53, 0);
             this.panelControl1.Name = "panelControl1";
-            this.panelControl1.Size = new System.Drawing.Size(322, 75);
+            this.panelControl1.Size = new System.Drawing.Size(392, 75);
             this.panelControl1.TabIndex = 0;
             // 
             // btnBusqPuesto
@@ -198,15 +198,15 @@
             this.barDockControl1.Dock = System.Windows.Forms.DockStyle.Top;
             this.barDockControl1.Location = new System.Drawing.Point(0, 0);
             this.barDockControl1.Manager = this.barManager2;
-            this.barDockControl1.Size = new System.Drawing.Size(375, 0);
+            this.barDockControl1.Size = new System.Drawing.Size(445, 0);
             // 
             // barDockControl2
             // 
             this.barDockControl2.CausesValidation = false;
             this.barDockControl2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.barDockControl2.Location = new System.Drawing.Point(0, 448);
+            this.barDockControl2.Location = new System.Drawing.Point(0, 473);
             this.barDockControl2.Manager = this.barManager2;
-            this.barDockControl2.Size = new System.Drawing.Size(375, 29);
+            this.barDockControl2.Size = new System.Drawing.Size(445, 29);
             // 
             // barDockControl3
             // 
@@ -214,15 +214,15 @@
             this.barDockControl3.Dock = System.Windows.Forms.DockStyle.Left;
             this.barDockControl3.Location = new System.Drawing.Point(0, 0);
             this.barDockControl3.Manager = this.barManager2;
-            this.barDockControl3.Size = new System.Drawing.Size(53, 448);
+            this.barDockControl3.Size = new System.Drawing.Size(53, 473);
             // 
             // barDockControl5
             // 
             this.barDockControl5.CausesValidation = false;
             this.barDockControl5.Dock = System.Windows.Forms.DockStyle.Right;
-            this.barDockControl5.Location = new System.Drawing.Point(375, 0);
+            this.barDockControl5.Location = new System.Drawing.Point(445, 0);
             this.barDockControl5.Manager = this.barManager2;
-            this.barDockControl5.Size = new System.Drawing.Size(0, 448);
+            this.barDockControl5.Size = new System.Drawing.Size(0, 473);
             // 
             // gridLookUpEdit1View
             // 
@@ -262,7 +262,7 @@
             this.panelControl2.Location = new System.Drawing.Point(53, 75);
             this.panelControl2.Name = "panelControl2";
             this.panelControl2.Padding = new System.Windows.Forms.Padding(10);
-            this.panelControl2.Size = new System.Drawing.Size(322, 373);
+            this.panelControl2.Size = new System.Drawing.Size(392, 398);
             this.panelControl2.TabIndex = 5;
             // 
             // gridControl1
@@ -272,7 +272,7 @@
             this.gridControl1.MainView = this.gridView1;
             this.gridControl1.MenuManager = this.barManager2;
             this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(298, 349);
+            this.gridControl1.Size = new System.Drawing.Size(368, 374);
             this.gridControl1.TabIndex = 0;
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
@@ -284,6 +284,7 @@
             this.gridColumn2});
             this.gridView1.GridControl = this.gridControl1;
             this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsView.ShowGroupPanel = false;
             // 
             // gridColumn1
             // 
@@ -304,13 +305,16 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(375, 477);
+            this.ClientSize = new System.Drawing.Size(445, 502);
             this.Controls.Add(this.panelControl2);
             this.Controls.Add(this.panelControl1);
             this.Controls.Add(this.barDockControl3);
             this.Controls.Add(this.barDockControl5);
             this.Controls.Add(this.barDockControl2);
             this.Controls.Add(this.barDockControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Frm_Cuadrilla";
             this.Text = "Frm_Cuadrilla";
             ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
